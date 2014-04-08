e414ef11eeae45464b6be3820ec41577e95a2b30
iSage <isage.dna@gmail.com>
2014-04-08 12:30:31 +0400

    cleanup bufio

--- common/bufio.cpp
+++ common/bufio.cpp
@@ -32,6 +32,16 @@ uint16_t read_U16(const uint8_t **data, const uint8_t *data_end)
 	return (ptr[1] << 8) | ptr[0];
 }
 
+uint32_t read_U24(const uint8_t **data, const uint8_t *data_end)
+{
+	uint32_t a, b;
+	
+	a = read_U16(data, data_end);
+	b = read_U8(data, data_end);
+	
+	return a | (b << 16);
+}
+
 uint32_t read_U32(const uint8_t **data, const uint8_t *data_end)
 {
 	const uint8_t *ptr = *data;
@@ -44,58 +54,4 @@ uint32_t read_U32(const uint8_t **data, const uint8_t *data_end)
 	
 	*data = (ptr + 4);
 	return (ptr[3] << 24) | (ptr[2] << 16) | (ptr[1] << 8) | ptr[0];
-}
-
-/*
-void c------------------------------() {}
-*/
-
-void write_U8(DBuffer *buffer, uint8_t data)
-{
-	buffer->AppendData((uint8_t *)&data, 1);
-}
-
-void write_U16(DBuffer *buffer, uint16_t data)
-{
-	buffer->AppendData((uint8_t *)&data, 2);
-}
-
-void write_U32(DBuffer *buffer, uint32_t data)
-{
-	buffer->AppendData((uint8_t *)&data, 4);
-}
-
-void write_U64(DBuffer *buffer, uint64_t data)
-{
-	buffer->AppendData((uint8_t *)&data, 8);
-}
-
-void write_F32(DBuffer *buffer, float data)
-{
-	buffer->AppendData((uint8_t *)&data, 4);
-}
-
-void write_F64(DBuffer *buffer, double data)
-{
-	buffer->AppendData((uint8_t *)&data, 8);
-}
-
-/*
-void c------------------------------() {}
-*/
-
-uint32_t read_U24(const uint8_t **data, const uint8_t *data_end)
-{
-	uint32_t a, b;
-	
-	a = read_U16(data, data_end);
-	b = read_U8(data, data_end);
-	
-	return a | (b << 16);
-}
-
-void write_U24(DBuffer *buffer, uint32_t data)
-{
-	write_U16(buffer, data);
-	write_U8(buffer, data >> 16);
 }
--- common/bufio.h
+++ common/bufio.h
@@ -5,14 +5,7 @@
 
 uint8_t read_U8(const uint8_t **data, const uint8_t *data_end);
 uint16_t read_U16(const uint8_t **data, const uint8_t *data_end);
-uint32_t read_U32(const uint8_t **data, const uint8_t *data_end);
-void write_U8(DBuffer *buffer, uint8_t data);
-void write_U16(DBuffer *buffer, uint16_t data);
-void write_U32(DBuffer *buffer, uint32_t data);
-void write_U64(DBuffer *buffer, uint64_t data);
-void write_F32(DBuffer *buffer, float data);
-void write_F64(DBuffer *buffer, double data);
 uint32_t read_U24(const uint8_t **data, const uint8_t *data_end);
-void write_U24(DBuffer *buffer, uint32_t data);
+uint32_t read_U32(const uint8_t **data, const uint8_t *data_end);
 
 #endif
