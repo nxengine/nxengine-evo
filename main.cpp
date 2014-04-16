1e510c2f102643aecf3826559bc864ae0be7ac98
iSage <isage.dna@gmail.com>
2014-04-17 02:51:32 +0400

    unused

--- main.cpp
+++ main.cpp
@@ -27,7 +27,6 @@ using namespace Graphics;
 const char *data_dir = "data";
 const char *stage_dir = "data/Stage";
 const char *pic_dir = "endpic";
-const char *nxdata_dir = ".";
 
 int fps = 0;
 static int fps_so_far = 0;
