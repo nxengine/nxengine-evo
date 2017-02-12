e5c86d1374366d6f6725cf0eac4f8ea36c003248
iSage <isage.dna@gmail.com>
2017-02-12 03:56:46 +0300

    Fix packing and bundle libs

--- travis/osx.package.sh
+++ travis/osx.package.sh
@@ -1,5 +1,6 @@
-brew install unzip
-brew install zip
+brew install dylibbundler
+
+#prepare
 mkdir release
 cd release
 wget http://www.cavestory.org/downloads/cavestoryen.zip
@@ -8,16 +9,25 @@ mkdir NXEngine.app
 mkdir NXEngine.app/Contents
 mkdir NXEngine.app/Contents/MacOS
 mkdir NXEngine.app/Contents/Resources
+
+#prepare and copy data
 cp ../bin/extract CaveStory/Doukutsu.exe
-cp ../bin/nx NXEngine.app/Contents/NXEngine
+cp ../bin/nx NXEngine.app/Contents/MacOS/NXEngine
 cd CaveStory
 ./extract
 cd ..
 cp -r CaveStory/data NXEngine.app/Contents/Resources
 cp -r ../data NXEngine.app/Contents/Resources
-cp font.ttf NXEngine.app/Contents/Resources
-cp smallfont.bmp NXEngine.app/Contents/Resources
-cp sprites.sif NXEngine.app/Contents/Resources
-cp tilekey.dat NXEngine.app/Contents/Resources
+cp ../font.ttf NXEngine.app/Contents/Resources
+cp ../smallfont.bmp NXEngine.app/Contents/Resources
+cp ../sprites.sif NXEngine.app/Contents/Resources
+cp ../tilekey.dat NXEngine.app/Contents/Resources
+
+#bundle libs
+dylibbundler -b -x NXEngine.app/Contents/MacOS/NXEngine -d NXEngine.app/Contents/libs-intel -od -p @executable_path/../libs-intel
+
+#create dmg
+
+#upload
 zip -r NXEngine.zip NXEngine.app
 curl --upload-file NXEngine.zip https://transfer.sh/nxengine
