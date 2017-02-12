b127ceef7e91cd36df7b54372b2ae84ad488faac
iSage <isage.dna@gmail.com>
2017-02-12 03:00:38 +0300

    Initial travis-ci integration

---
+++ travis/linux.install.sh
@@ -0,0 +1,7 @@
+# sdl2
+sudo add-apt-repository ppa:zoogie/sdl2-snapshots -y
+
+sudo apt-get update -qq
+sudo apt-get install -qq libsdl2-dev
+sudo apt-get install -qq libsdl2-ttf-dev
+sudo apt-get install -qq libpng-dev
---
+++ travis/osx.install.sh
@@ -0,0 +1,4 @@
+brew update
+brew install sdl2
+brew install sdl2_ttf
+brew install libpng
