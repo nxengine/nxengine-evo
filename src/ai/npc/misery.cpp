41ed2d6ec8659087992a9e406307f0cca4d560aa
iSage <isage.dna@gmail.com>
2017-11-14 15:48:37 +0300

    Bubble should delete Toroko, not just make her invisible. Fixes #19

--- src/ai/npc/misery.cpp
+++ src/ai/npc/misery.cpp
@@ -168,7 +168,10 @@ Object *target;
 				sound(SND_BUBBLE);
 				
 				if ((target = mbubble_find_target()))
-					target->invisible = true;
+				{
+					target->Delete();
+//					target->invisible = true;
+				}
 			}
 		break;
 		
