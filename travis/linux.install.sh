d77d007181a877599d944b528bcd2f61f3ebc2f7
iSage <isage.dna@gmail.com>
2017-12-05 02:04:44 +0300

    And some more build fixes

--- travis/linux.install.sh
+++ travis/linux.install.sh
@@ -1,10 +1,10 @@
 # sdl2
 sudo add-apt-repository ppa:zoogie/sdl2-snapshots -y
 # gcc
-[[ -z "${INSTALLGCC}" ]] && sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+[[ -z "${INSTALLGCC}" ]] || sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
 sudo apt-get update -qq
 sudo apt-get install -qq libsdl2-dev
 sudo apt-get install -qq libsdl2-mixer-dev
 sudo apt-get install -qq libpng-dev
-[[ -z "${INSTALLGCC}" ]] && sudo apt-get install -qq g++-6
+[[ -z "${INSTALLGCC}" ]] || sudo apt-get install -qq g++-6
