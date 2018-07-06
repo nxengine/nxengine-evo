e173e7f1293c4eb78cfa8568b79b0cb9abfeff23
JTE <catgirl@goddess.moe>
2018-07-05 23:50:55 -0500

    stagename needs to be static
    because it has a chance that translation will just return it directly, of course.

--- src/map.cpp
+++ src/map.cpp
@@ -1106,7 +1106,8 @@ void map_ChangeTileWithSmoke(int x, int y, int newtile, int nclouds, bool boomfl
 
 const std::string& map_get_stage_name(int mapno)
 {
-	std::string stagename = (std::string)"stage_" + stages[mapno].filename;
+	static std::string stagename;
+	stagename = (std::string)"stage_" + stages[mapno].filename;
 	if (_(stagename) == stagename)
 	{
 		if (mapno == STAGE_KINGS)
