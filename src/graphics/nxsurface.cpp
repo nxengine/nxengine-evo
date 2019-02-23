88a07ad734ed08c6ab8eeab7599621db16abe656
iSage <isage.dna@gmail.com>
2019-02-23 07:24:49 +0300

    Ugh. Vita won't clear the screen properly

--- src/graphics/nxsurface.cpp
+++ src/graphics/nxsurface.cpp
@@ -328,7 +328,7 @@ void NXSurface::Clear(uint8_t r, uint8_t g, uint8_t b)
     SetAsTarget(true);
 
   SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
-  // SDL_RenderFillRect(renderer, NULL);
+  SDL_RenderFillRect(renderer, NULL);
   SDL_RenderClear(renderer);
 
   if (this != screen)
