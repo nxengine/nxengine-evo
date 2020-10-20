fb32cb53c29988b59421895f3451a2beb1c33591
Alexander Schlarb <alexander@ninetailed.ninja>
2020-10-20 21:46:44 +0200

    Fall back to `curl` in download script if `wget` is not available

--- build-scripts/utils/common.download-extern.sh
+++ build-scripts/utils/common.download-extern.sh
@@ -8,6 +8,21 @@
 #  * File "${APP_ID}.json" must contain the Flatpak builder manifest
 #
 # Call as: build-scripts/utils/common.download-extern.sh
+set -eu
+
+# Check for required non-POSIX commands
+if type wget >/dev/null 2>&1;
+then
+	DOWNLOAD_CMD="wget -O"
+elif type curl >/dev/null 2>&1;
+then
+	DOWNLOAD_CMD="curl -Lo"
+else
+	echo 'Neither of commands `wget` and `curl` (HTTPS downloader) found, aborting!' >&2 && exit 1
+fi
+type jq        >/dev/null 2>&1 || ( echo 'Required command `jq` (JSON query) not found, aborting!'        >&2 && exit 1 )
+type sha256sum >/dev/null 2>&1 || ( echo 'Required command `sha25sum` (file hasher) not found, aborting!' >&2 && exit 1 )
+type unzip     >/dev/null 2>&1 || ( echo 'Required command `unzip` (ZIP extractor) not found, aborting!'  >&2 && exit 1 )
 
 
 # Download, verify and extract external resources mentioned in the Flatpak builder manifest
@@ -18,7 +33,8 @@ do
 	filepath="extern/${sha256}.${origname#*.}"
 	
 	# Download and verify archive file
-	test -e "${filepath}" || wget "${url}" -O "${filepath}"
+	test -e "${filepath}" || ${DOWNLOAD_CMD} "${filepath}" "${url}"
+	
 	echo "${sha256}  ${filepath}" | sha256sum -c
 	
 	# Extract archive and move it to the same location that it would end up in the Flatpak build
