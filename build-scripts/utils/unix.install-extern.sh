861cab56f4a691c455c64a34c0e7b563067f8885
Alexander Schlarb <alexander@ninetailed.ninja>
2020-10-20 14:38:07 +0200

    Restructure build scripts as suggested in https://github.com/nxengine/nxengine-evo/issues/187#issuecomment-712444606

--- build-scripts/utils/unix.install-extern.sh
+++ build-scripts/utils/unix.install-extern.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
-# Expects to be called during the Flatpak or AppImage build process
+#!/bin/sh
+# Install external dependencies needed to complete the install step
 #
+# Expects to be called during the Flatpak or AppImage build process.
 # Because of this, the following things are expected about the environment:
 #  * Current working directory must be at the GIT checkout of the project root directory
 #  * Directory `extern/Translations` must contain the preprocessed and extracted translations (one subdirectory for each language)
 #  * Directory `extern/CaveStory` must contain the extracted Windows game binary
 #
 # Call as: build-scripts/unix.install-extras.sh [target-path:/usr] [/path/to/nxextract]
-set -eu -o pipefail
+set -eu
 TARGET="${1}"
 NXEXTRACT="$(readlink -f "${2}")"
 
