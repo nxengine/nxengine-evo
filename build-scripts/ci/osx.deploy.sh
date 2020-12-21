3d6393470a130e697eb6f945a579ae08f87e9a2f
Ivan Epifanov <isage.dna@gmail.com>
2020-12-21 20:16:13 +0300

    remove ci debug

--- build-scripts/ci/osx.deploy.sh
+++ build-scripts/ci/osx.deploy.sh
@@ -2,6 +2,4 @@ if [ ${APPVEYOR_REPO_TAG} == "true" ]; then
   brew install create-dmg dylibbundler jq
   build-scripts/build-macos.sh --no-build
   appveyor PushArtifact NXEngine-Evo-${APPVEYOR_REPO_TAG_NAME}-OSX.dmg -DeploymentName osxbuild
-
-  sqlite3 "$HOME/Library/Application Support/com.apple.TCC/TCC.db" ".dump access;"
 fi
