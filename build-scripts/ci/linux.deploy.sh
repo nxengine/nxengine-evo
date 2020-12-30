d67790187e3f8a7fa320650e22876b3ebd53f6c5
Ivan Epifanov <isage.dna@gmail.com>
2020-12-31 00:12:36 +0300

    Fix linux deploy

--- build-scripts/ci/linux.deploy.sh
+++ build-scripts/ci/linux.deploy.sh
@@ -3,5 +3,5 @@ if [ ${APPVEYOR_REPO_TAG} == "true" ]; then
   sudo apt install -yqq libxml2-utils jq appstream appstream-util
   build-scripts/build-appimage.sh --no-build
 
-  appveyor PushArtifact "NXEngine-${APPVEYOR_REPO_TAG}-Linux-x86_64.AppImage" -DeploymentName appimagebuild
+  appveyor PushArtifact "NXEngine-${APPVEYOR_REPO_TAG_NAME}-Linux-x86_64.AppImage" -DeploymentName appimagebuild
 fi
